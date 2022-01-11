8c543676ebe68dc0ab0a2c3369065a806ff3e9d5
Chinjan Patel <chinjan@vetron.in>
2022-01-11 14:23:40 +0530

    Sign up
    - Submit
    
    BUG
    3h 53m

--- src/app/views/sessions/sign-up/sign-up.component.ts
+++ src/app/views/sessions/sign-up/sign-up.component.ts
@@ -58,6 +58,46 @@ export class SignUpComponent {
     this.loading = true;
     this.submitted = true;
     this.loadingText = 'Signing up...';
+    if (this.form.valid) {
+      const {username, password, email, name} = this.form.value;
+      this.amplifyAuth.signUp(username, password, {email, name})
+        .then((response: ISignUpResult) => {
+          if (response.codeDeliveryDetails) {
+            const customResponse = {
+              user: response.user,
+              CodeDeliveryDetails: response.codeDeliveryDetails
+            };
+            const bottomSheet = this.bottomSheet.open(CompleteSignUpComponent, {
+              data: {
+                other: {
+                  username
+                },
+                alreadyHasVerificationCode: customResponse
+              }
+            });
+            bottomSheet.afterDismissed().subscribe(() => {
+              this.router.navigate(['/', 'sessions', 'sign-in']);
+            });
+          }
+        })
+        .catch(error => {
+          if (error.code === 'UsernameExistsException') {
+            this.partialLoadingText = 'Try Signing in';
+            this.partialLoading = !this.partialLoading;
+            this.partialLoadingClass = 'btn-primary btn-rounded btn-block';
+            setTimeout(() => {
+              this.partialLoadingText = 'Sign In';
+              this.partialLoadingClass = 'btn btn-rounded btn-outline-primary btn-outline-email btn-block btn-icon-text';
+            }, 2000);
+          }
+        })
+        .finally(() => {
+          this.loading = false;
+          this.partialLoading = false;
+        });
+    } else {
+      this.loading = false;
+    }
   }
 
 }
